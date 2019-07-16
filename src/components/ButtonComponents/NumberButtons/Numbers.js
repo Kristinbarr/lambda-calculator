9d6a15a344aa82552a81632e69db9256314cc35c
Kristin Barr <kristinbarr@gmail.com>
2019-07-16 18:39:44 -0400

    components connected and displaying

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,17 @@
 import React from "react";
-
+import NumberButton from './NumberButton';
 //import any components needed
-
+import data from '../../../data.js';
 //Import your array data to from the provided data file
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      {data.numbers.map(number => <NumberButton number={number} /> )}
+      {/* STEP 3 - Use .map() to iterate over your array data and return a button component matching the name on the provided file. Pass it any props needed by the child component*/}
     </div>
   );
 };
+
+export default Numbers
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,17 @@
 import React from "react";
-
+import SpecialButton from './SpecialButton'
 //import any components needed
-
+import data from '../../../data'
 //Import your array data to from the provided data file
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      {data.specials.map(special => <SpecialButton special={special} />)}
+      {/* STEP 3 - Use .map() to iterate over your array data and return a button component matching the name on the provided file. Pass it any props needed by the child component*/}
     </div>
   );
 };
+
+export default Specials;
